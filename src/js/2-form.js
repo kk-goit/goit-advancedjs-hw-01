62b8a4d6018faf2b72f6efca48f07ea56c3207fd
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2024-09-29 14:00:43 +0300

    Tasks complited

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const storageItemName = "feedback-form-state";
+
+let formData = {
+    email: "",
+    message: ""
+};
+
+const lStorage = window.localStorage;
+const form = document.querySelector('.feedback-form');
+const fEmail = form.elements.email;
+const fMsg = form.elements.message;
+
+// try load and preset stored data
+const fData = lStorage.getItem(storageItemName);
+if (fData) { 
+    formData = JSON.parse(fData);
+    fEmail.value = formData.email;
+    fMsg.value = formData.message;
+}
+
+// add event listeners
+form.addEventListener("input", (evt) => {
+    formData[evt.target.name] = evt.target.value;
+    lStorage.setItem(storageItemName, JSON.stringify(formData));
+  });
+  
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    if (fEmail.value && fMsg.value) { 
+        console.log(formData);
+        lStorage.removeItem(storageItemName);
+        formData.email = "";
+        formData.message = "";
+        form.reset();
+    } else {
+        window.alert("Fill please all fields");
+    }
+  });
